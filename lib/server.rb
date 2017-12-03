bab0ad4d734a197544bb5f062d40a0905ad0a7a3
Pete Nicholls <pete@metanation.com>
2017-12-02 19:43:19 -0800

    Extract some methods from Server

--- lib/server.rb
+++ lib/server.rb
@@ -5,38 +5,55 @@ require 'leaderboard'
 require 'hack'
 
 class Server
-  attr_reader :config
+  NOT_AUTHORIZED_RESPONSE = [401, {}, ['Not authorized']]
+
+  attr_reader :config, :request, :hack
 
   def initialize(config)
     @config = config
   end
 
   def call(env)
-    request = Rack::Request.new(env)
+    @request = Rack::Request.new(env)
 
-    if request.params.fetch('token') != config.verification_token
-      [401, {}, ['Not authorized, invalid verification token']]
+    if token_invalid?
+      NOT_AUTHORIZED_RESPONSE
     else
-      message = request.params.fetch('text')
-
-      if message.strip.split(/\s/).first == 'leaderboard'
+      if leaderboard_requested?
         body = Leaderboard.new(config.database).to_markdown
       else
-        victim_id = request.params.fetch('user_id')
-        attacker_id = SlackUserID.parse(message)
-
-        hack = Hack.new(attacker_id: attacker_id, victim_id: victim_id)
-        hack.persist!(config.database)
-
+        record_hack!
         body = hack.acknowledgement_message(config.acknowledgement)
       end
 
-      json = JSON.generate(
-        response_type: "in_channel",
-        text: body
-      )
-
-      [200, { 'Content-Type' => 'application/json' }, [json]]
+      json_response(response_type: "in_channel", text: body)
     end
+  end
+
+  private
+
+  def record_hack!
+    victim_id = request.params.fetch('user_id')
+    attacker_id = SlackUserID.parse(slash_message_content)
+
+    @hack = Hack.new(attacker_id: attacker_id, victim_id: victim_id)
+    hack.persist!(config.database)
+  end
+
+  def json_response(status: 200, **payload)
+    body = JSON.generate(payload)
+    [status, { 'Content-Type' => 'application/json' }, [body]]
+  end
+
+  def token_invalid?
+    request.params.fetch('token') != config.verification_token
+  end
+
+  def slash_message_content
+    request.params.fetch('text').strip
+  end
+
+  def leaderboard_requested?
+    slash_message_content.split(/\s/).first == 'leaderboard'
   end
 end
